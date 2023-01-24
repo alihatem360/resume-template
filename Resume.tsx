7d06b92559709544d5032804978dbc323cf574fa
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2023-01-24 10:14:06 +0200

    update title

--- Resume.tsx
+++ Resume.tsx
@@ -7,7 +7,7 @@ export const Resume: React.FC = () => {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>John Doe - Frontend Developer</title>
+        <title>Ali Hatem - Frontend Developer</title>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
